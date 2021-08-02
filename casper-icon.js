6f5d8d0f3f665ff8a3c03ea8b019ea621d08f234
Eurico Inocencio <eurico.inocencio@gmail.com>
2021-08-02 11:22:43 +0100

    casper icon litification in progress

--- casper-icon.js
+++ casper-icon.js
@@ -1,47 +1,37 @@
-import './casper-iconset-fa-light.js';
-import './casper-iconset-fa-solid.js';
-import './casper-iconset-fa-regular.js';
+/*
+  - Copyright (c) 2014-2021 Cloudware S.A. All rights reserved.
+  -
+  - This file is part of casper-icon.
+  -
+  - casper-icon is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as published by
+  - the Free Software Foundation, either version 3 of the License, or
+  - (at your option) any later version.
+  -
+  - casper-icon  is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with casper-icon.  If not, see <http://www.gnu.org/licenses/>.
+  -
+ */
 
-import { templatize } from '@polymer/polymer/lib/utils/templatize.js';
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import { LitElement, css } from 'lit';
+import { unsafeSVG }       from 'lit/directives/unsafe-svg.js';
 
-class CasperIcon extends PolymerElement {
+/**
+ * Little more than a glorified SVG element, with square aspect ratio,
+ * that uses an in memory cache to keep a collection of often used SVG.
+ * 
+ * To improve loading performance of application's often used fa-awsome icons
+ * are grouped into to three dynmically loaded predefined sets organized by icon family  
+ */
+export class CasperIcon extends LitElement {
 
-  static get template () {
-    return html`
-      <style>
-        :host {
-          width: 18px;
-          height: 18px;
-          display: inline-flex;
-        }
-
-        :host([hidden]) {
-          display: none !important;
-        }
-
-        svg {
-          width: 100%;
-          height: 100%;
-          fill: currentColor;
-          pointer-events: none;
-        }
-      </style>
-    `;
-  }
-
-  static get properties () {
+  static get properties() {
     return {
-      /**
-       * Flag that states if the component should be hidden or not.
-       *
-       * @type {Boolean}
-       */
-      hidden: {
-        type: Boolean,
-        value: false,
-        reflectToAttribute: true
-      },
       /**
        * The icon's name.
        *
@@ -49,38 +39,95 @@ class CasperIcon extends PolymerElement {
        */
       icon: {
         type: String,
-        observer: '__iconChanged'
-      },
+        reflect: true
+      }
     };
   }
 
-  async __iconChanged (iconFullName) {
-    // Clear the exisiting if there is any.
-    const existingIcon = this.shadowRoot.querySelector('svg');
-    if (existingIcon) {
-      this.shadowRoot.removeChild(existingIcon);
+  static styles = css`
+    :host {
+      width: 18px;
+      height: 18px;
+      display: inline-flex;
     }
 
-    if (!iconFullName) return;
-
-    const [iconset, icon] = iconFullName.split(':');
-
-    let iconsetElement = document.head.querySelector(`casper-iconset[name="${iconset}"]`);
-    if (!iconsetElement) {
-      console.error(`The requested iconset - ${iconset} - does not exist.`);
-      return;
+    :host([hidden]) {
+      display: none !important;
     }
 
-    const template = iconsetElement.getTemplateForIcon(icon);
-    if (!template) {
-      console.error(`The requested icon - ${icon} - does not exist.`);
-      return;
+    svg {
+      width: 100%;
+      height: 100%;
+      fill: currentColor;
+      pointer-events: none;
     }
+  `;
+      
+  /**
+   * Global registry or cache of template literals tagged with lit's svg
+   */
+  static _registry = new Map();
 
-    const templateClass = templatize(template);
-
-    this.shadowRoot.appendChild(new templateClass().root);
+  static register (name, svgTag) {
+    CasperIcon._registry.set(name, svgTag);
   }
+
+  shouldUpdate (changedProperties) {
+    if ( changedProperties.has('icon') ) {
+      // fetch the SVG literal from the registry ...
+      this._icon = CasperIcon._registry.get(this.icon); 
+
+      if ( this._icon !== undefined ) {
+        // ... we already have the icon in stock, let the update proceed ...
+        return true; 
+      } else {
+        // ... not found, trigger the async load process the stop this render ...
+        this._lazyLoadIcon();
+        return false;
+      }
+    }
+    return true;
+  }
+
+  render () {
+    console.log('render called');
+    return this._icon;
+  }
+
+  async _lazyLoadIcon () {
+    // first check if it belongs to a icon set ...
+    const [iconSet, iconName] = this.icon.split(':');
+
+    if ( iconName ) {
+      // ... it's in a predefined set, so we need to import the set.
+      try {       
+        await import(`./casper-iconset-${iconSet}.js`);
+
+        // ... if all goes well the set should have registered the new icon ...
+        this._icon = CasperIcon._registry.get(this.icon);
+        if ( ! this._icon ) {
+          console.warn(`CasperIcon: there is no icon named '${iconName}' in the predefined set '${iconSet}'!!!`);
+        }
+      } catch (e) {
+        // ... ignore just blank the icon ...
+        this._icon = undefined;
+        console.warn(`CasperIcon: loading of predefined set '${iconSet}' failed for icon named '${iconName}'!!!`);
+      }
+    } else {
+      // ... the icon does not belong to a set, so it will be loaded from a SVG file ...
+      const lazySvg = await fetch(`${this.icon[0] === '/' ? this.icon : `/static/icons/${this.icon}`}.svg`);
+      if ( lazySvg.ok ) {
+        this._icon = unsafeSVG(await lazySvg.text());
+        CasperIcon.register(this.icon, this._icon);
+      } else {
+        // ... bummer, someone is screwing up the file does exists where it should ...
+        this._icon = undefined;
+        console.warn(`CasperIcon: unable to load icon named '${this.icon}'`);
+      }
+    }
+    this.requestUpdate();
+  }
+
 }
 
 window.customElements.define('casper-icon', CasperIcon);
--- casper-iconset-fa-regular.js
+++ casper-iconset-fa-regular.js
@@ -1,69 +1,72 @@
-import './casper-iconset.js';
+/*
+  - Copyright (c) 2014-2021 Cloudware S.A. All rights reserved.
+  -
+  - This file is part of casper-icon.
+  -
+  - casper-icon is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as published by
+  - the Free Software Foundation, either version 3 of the License, or
+  - (at your option) any later version.
+  -
+  - casper-icon  is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with casper-icon.  If not, see <http://www.gnu.org/licenses/>.
+  -
+ */
 
-const template = document.createElement('template');
-template.innerHTML = /* html */`
-  <casper-iconset name="fa-regular">
+import { svg }        from 'lit';
+import { CasperIcon } from './casper-icon.js';
 
-    <template id="angle-down">
+CasperIcon.register('fa-regular:angle-down', svg`
       <svg viewBox="0 0 320 512">
         <path d="M151.5 347.8L3.5 201c-4.7-4.7-4.7-12.3 0-17l19.8-19.8c4.7-4.7 12.3-4.7 17 0L160 282.7l119.7-118.5c4.7-4.7 12.3-4.7 17 0l19.8 19.8c4.7 4.7 4.7 12.3 0 17l-148 146.8c-4.7 4.7-12.3 4.7-17 0z"/>
-      </svg>
-    </template>
+      </svg>`);
 
-    <template id="angle-left">
+CasperIcon.register('fa-regular:angle-left', svg`
       <svg viewBox="0 0 192 512">
         <path d="M4.2 247.5L151 99.5c4.7-4.7 12.3-4.7 17 0l19.8 19.8c4.7 4.7 4.7 12.3 0 17L69.3 256l118.5 119.7c4.7 4.7 4.7 12.3 0 17L168 412.5c-4.7 4.7-12.3 4.7-17 0L4.2 264.5c-4.7-4.7-4.7-12.3 0-17z"/>
-      </svg>
-    </template>
+      </svg>`);
 
-    <template id="angle-right">
+CasperIcon.register('fa-regular:angle-right', svg`
       <svg viewBox="0 0 192 512">
         <path d="M187.8 264.5L41 412.5c-4.7 4.7-12.3 4.7-17 0L4.2 392.7c-4.7-4.7-4.7-12.3 0-17L122.7 256 4.2 136.3c-4.7-4.7-4.7-12.3 0-17L24 99.5c4.7-4.7 12.3-4.7 17 0l146.8 148c4.7 4.7 4.7 12.3 0 17z"/>
-      </svg>
-    </template>
+      </svg>`);
 
-    <template id="check">
+CasperIcon.register('fa-regular:check', svg`
       <svg viewBox="0 0 512 512">
         <path d="M435.848 83.466L172.804 346.51l-96.652-96.652c-4.686-4.686-12.284-4.686-16.971 0l-28.284 28.284c-4.686 4.686-4.686 12.284 0 16.971l133.421 133.421c4.686 4.686 12.284 4.686 16.971 0l299.813-299.813c4.686-4.686 4.686-12.284 0-16.971l-28.284-28.284c-4.686-4.686-12.284-4.686-16.97 0z"/>
-      </svg>
-    </template>
+      </svg>`);
 
-    <template id="edit">
+CasperIcon.register('fa-regular:edit', svg`
       <svg viewBox="0 0 576 512">
         <path d="M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z"/>
-      </svg>
-    </template>
+      </svg>`);
 
-    <template id="exclamation-circle">
+CasperIcon.register('fa-regular:exclamation-circle', svg`
       <svg viewBox="0 0 512 512">
         <path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm42-104c0 23.159-18.841 42-42 42s-42-18.841-42-42 18.841-42 42-42 42 18.841 42 42zm-81.37-211.401l6.8 136c.319 6.387 5.591 11.401 11.985 11.401h41.17c6.394 0 11.666-5.014 11.985-11.401l6.8-136c.343-6.854-5.122-12.599-11.985-12.599h-54.77c-6.863 0-12.328 5.745-11.985 12.599z"/>
-      </svg>
-    </template>
+      </svg>`);
 
-    <template id="search">
+CasperIcon.register('fa-regular:search', svg`
       <svg viewBox="0 0 512 512">
         <path d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"/>
-      </svg>
-    </template>
+      </svg>`);
 
-    <template id="sign-in-alt">
+CasperIcon.register('fa-regular:sign-in-alt', svg`
       <svg viewBox="0 0 512 512">
         <path d="M144 112v51.6H48c-26.5 0-48 21.5-48 48v88.6c0 26.5 21.5 48 48 48h96v51.6c0 42.6 51.7 64.2 81.9 33.9l144-143.9c18.7-18.7 18.7-49.1 0-67.9l-144-144C195.8 48 144 69.3 144 112zm192 144L192 400v-99.7H48v-88.6h144V112l144 144zm80 192h-84c-6.6 0-12-5.4-12-12v-24c0-6.6 5.4-12 12-12h84c26.5 0 48-21.5 48-48V160c0-26.5-21.5-48-48-48h-84c-6.6 0-12-5.4-12-12V76c0-6.6 5.4-12 12-12h84c53 0 96 43 96 96v192c0 53-43 96-96 96z"/>
-      </svg>
-    </template>
+      </svg>`);
 
-    <template id="sync">
+CasperIcon.register('fa-regular:sync', svg`
       <svg viewBox="0 0 512 512">
         <path d="M500 8h-27.711c-6.739 0-12.157 5.548-11.997 12.286l2.347 98.575C418.212 52.043 342.256 8 256 8 134.813 8 33.933 94.924 12.296 209.824 10.908 217.193 16.604 224 24.103 224h28.576c5.674 0 10.542-3.982 11.737-9.529C83.441 126.128 161.917 60 256 60c79.545 0 147.942 47.282 178.676 115.302l-126.39-3.009c-6.737-.16-12.286 5.257-12.286 11.997V212c0 6.627 5.373 12 12 12h192c6.627 0 12-5.373 12-12V20c0-6.627-5.373-12-12-12zm-12.103 280h-28.576c-5.674 0-10.542 3.982-11.737 9.529C428.559 385.872 350.083 452 256 452c-79.546 0-147.942-47.282-178.676-115.302l126.39 3.009c6.737.16 12.286-5.257 12.286-11.997V300c0-6.627-5.373-12-12-12H12c-6.627 0-12 5.373-12 12v192c0 6.627 5.373 12 12 12h27.711c6.739 0 12.157-5.548 11.997-12.286l-2.347-98.575C93.788 459.957 169.744 504 256 504c121.187 0 222.067-86.924 243.704-201.824 1.388-7.369-4.308-14.176-11.807-14.176z"/>
-      </svg>
-    </template>
+      </svg>`);
 
-    <template id="times">
+CasperIcon.register('fa-regular:times', svg`
       <svg viewBox="0 0 320 512">
         <path d="M207.6 256l107.72-107.72c6.23-6.23 6.23-16.34 0-22.58l-25.03-25.03c-6.23-6.23-16.34-6.23-22.58 0L160 208.4 52.28 100.68c-6.23-6.23-16.34-6.23-22.58 0L4.68 125.7c-6.23 6.23-6.23 16.34 0 22.58L112.4 256 4.68 363.72c-6.23 6.23-6.23 16.34 0 22.58l25.03 25.03c6.23 6.23 16.34 6.23 22.58 0L160 303.6l107.72 107.72c6.23 6.23 16.34 6.23 22.58 0l25.03-25.03c6.23-6.23 6.23-16.34 0-22.58L207.6 256z"/>
-      </svg>
-    </template>
-  </casper-iconset>
-`;
-
-document.head.appendChild(template.content);
+      </svg>`);
