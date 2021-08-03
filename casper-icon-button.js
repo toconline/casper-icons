b77814fc9267eb1078cde1432b25184694e42ff3
Eurico Inocencio <eurico.inocencio@gmail.com>
2021-08-03 10:10:56 +0100

    Initial litrification

--- casper-icon-button.js
+++ casper-icon-button.js
@@ -1,9 +1,104 @@
+/*
+  - Copyright (c) 2014-2021 Cloudware S.A. All rights reserved.
+  -
+  - This file is part of casper-icon.
+  -
+  - casper-icon is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as published by
+  - the Free Software Foundation, either version 3 of the License, or
+  - (at your option) any later version.
+  -
+  - casper-icon  is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with casper-icon.  If not, see <http://www.gnu.org/licenses/>.
+  -
+ */
+
+import { LitElement, css, html } from 'lit';
 import './casper-icon.js';
 import '@polymer/paper-ripple/paper-ripple.js';
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 
-class CasperIconButton extends PolymerElement {
+class CasperIconButton extends LitElement {
+
+  static styles = css`
+    :host {
+      width: 40px;
+      height: 40px;
+      padding: 8px;
+      display: flex;
+      user-select: none;
+      position: relative;
+      align-items: center;
+      justify-items: center;
+      box-sizing: border-box;
+      color: var(--casper-icon-button-color, var(--on-primary-color));
+      background-color: var(--casper-icon-button-background-color, var(--primary-color));
+      border: 1px solid var(--casper-icon-button-background-color, var(--primary-color));
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    casper-icon {
+      flex-shrink: 0;
+    }
+
+    :host([reverse]) {
+      color: var(--casper-icon-button-background-color, var(--primary-color));
+      background-color: var(--casper-icon-button-color, var(--on-primary-color));
+    }
+
+    :host([disabled]) {
+      border: none;
+      pointer-events: none;
+      color: var(--disabled-text-color);
+      background-color: var(--disabled-background-color);
+    }
+
+    :host([hidden]) {
+      display: none !important;
+    }
+
+    :host([with-text]) {
+      border-radius: 20px;
+    }
+
+    :host([with-text][disable-text-auto-width]) {
+      /*display: flex;*/
+    }
+
+    :host([with-text]:not([disable-text-auto-width])) {
+      width: auto;
+    }
+
+    :host(:not([with-text])) {
+      border-radius: 50%;
+    }
+
+    :host([with-text]) casper-icon {
+      margin-right: 5px;
+    }
+
+    :host(:not([with-text])) casper-icon {
+      width: 100%;
+      height: 100%;
+    }
+
+    /* Hover styling */
+    :host(:hover) {
+      cursor: pointer;
+      color: var(--casper-icon-button-background-color, var(--primary-color));
+      background-color: var(--casper-icon-button-color, var(--on-primary-color));
+    }
+
+    :host([reverse]:hover) {
+      color: var(--casper-icon-button-color, var(--on-primary-color));
+      background-color: var(--casper-icon-button-background-color, var(--primary-color));
+    }`;
 
   static get properties () {
     return {
@@ -22,8 +117,7 @@ class CasperIconButton extends PolymerElement {
        */
       disabled: {
         type: Boolean,
-        value: false,
-        reflectToAttribute: true
+        reflect: true
       },
       /**
        * Flag that states if the button should not set its width automatically to fit the content when there's text.
@@ -32,8 +126,8 @@ class CasperIconButton extends PolymerElement {
        */
       disableTextAutoWidth: {
         type: Boolean,
-        value: false,
-        reflectToAttribute: true
+        attribute: 'disable-text-auto-width',
+        reflect: true
       },
       /**
        * Flag that reversed the current style of the button.
@@ -42,8 +136,8 @@ class CasperIconButton extends PolymerElement {
        */
       reverse: {
         type: Boolean,
-        value: false,
-        reflectToAttribute: true
+        //value: false,
+        reflect: true
       },
       /**
        * Text that should appear on the button.
@@ -52,7 +146,8 @@ class CasperIconButton extends PolymerElement {
        */
       text: {
         type: String,
-        observer: '__textChanged'
+        reflect: true
+        //observer: '__textChanged' // TODO
       },
       /**
        * Flag that states if the button has text or is only an icon.
@@ -61,8 +156,8 @@ class CasperIconButton extends PolymerElement {
        */
       withText: {
         type: Boolean,
-        value: false,
-        reflectToAttribute: true
+        attribute: 'with-text',
+        reflect: true
       },
       /**
        * Flag that states if the button should have a border or not.
@@ -71,88 +166,33 @@ class CasperIconButton extends PolymerElement {
        */
       withBorder: {
         type: Boolean,
-        value: false,
-        reflectToAttribute: true
+        attribute: 'with-border',
+        reflect: true
       }
     };
   }
 
-  static get template () {
+  constructor () {
+    super();
+    this.disabled   = false;
+    this.reverse    = false;
+    this.withText   = false;
+    this.withBorder = false;
+    this.disableTextAutoWidth = false;
+  }
+
+  willUpdate (changedProperties) {
+    if ( changedProperties.has('text') ) {
+      console.log("**** ", this.text);
+      this.withText = !! this.text;
+    }
+  }
+
+  render () {
     return html`
-      <style>
-        :host {
-          width: 40px;
-          height: 40px;
-          padding: 8px;
-          display: flex;
-          user-select: none;
-          position: relative;
-          align-items: center;
-          justify-items: center;
-          box-sizing: border-box;
-          color: var(--casper-icon-button-color, var(--on-primary-color));
-          background-color: var(--casper-icon-button-background-color, var(--primary-color));
-          border: 1px solid var(--casper-icon-button-background-color, var(--primary-color));
-        }
-
-        :host([reverse]) {
-          color: var(--casper-icon-button-background-color, var(--primary-color));
-          background-color: var(--casper-icon-button-color, var(--on-primary-color));
-        }
-
-        :host([disabled]) {
-          border: none;
-          pointer-events: none;
-          color: var(--disabled-text-color);
-          background-color: var(--disabled-background-color);
-        }
-
-        :host([hidden]) {
-          display: none !important;
-        }
-
-        :host([with-text]) {
-          border-radius: 20px;
-        }
-
-        :host([with-text][disable-text-auto-width]) {
-          display: flex;
-          justify-content: center;
-        }
-
-        :host([with-text]:not([disable-text-auto-width])) {
-          width: fit-content !important;
-        }
-
-        :host(:not([with-text])) {
-          border-radius: 50%;
-        }
-
-        :host([with-text]) casper-icon {
-          margin-right: 5px;
-        }
-
-        :host(:not([with-text])) casper-icon {
-          width: 100%;
-          height: 100%;
-        }
-
-        /* Hover styling */
-        :host(:hover) {
-          cursor: pointer;
-          color: var(--casper-icon-button-background-color, var(--primary-color));
-          background-color: var(--casper-icon-button-color, var(--on-primary-color));
-        }
-
-        :host([reverse]:hover) {
-          color: var(--casper-icon-button-color, var(--on-primary-color));
-          background-color: var(--casper-icon-button-background-color, var(--primary-color));
-        }
-      </style>
-
       <paper-ripple></paper-ripple>
-      <casper-icon icon="[[icon]]" id="icon"></casper-icon>
-      [[text]]
+      <casper-icon icon=${this.icon} id="icon"></casper-icon>
+      ${this.text}
     `;
   }
 
