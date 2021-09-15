8323bb46148d414e7aeb7c08f49f54e152d44698
Eurico Inocencio <eurico.inocencio@gmail.com>
2021-09-15 17:03:09 +0100

    Improving the gallery copyright fixed my heart is broken, loading components still depend on polymer 3 yikes!!!!

--- casper-icon-gallery.js
+++ casper-icon-gallery.js
@@ -1,20 +1,20 @@
 /*
   - Copyright (c) 2014-2021 Cloudware S.A. All rights reserved.
   -
-  - This file is part of casper-icon.
+  - This file is part of casper-icons.
   -
-  - casper-icon is free software: you can redistribute it and/or modify
+  - casper-icons is free software: you can redistribute it and/or modify
   - it under the terms of the GNU Affero General Public License as published by
   - the Free Software Foundation, either version 3 of the License, or
   - (at your option) any later version.
   -
-  - casper-icon  is distributed in the hope that it will be useful,
+  - casper-icons  is distributed in the hope that it will be useful,
   - but WITHOUT ANY WARRANTY; without even the implied warranty of
   - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   - GNU General Public License for more details.
   -
   - You should have received a copy of the GNU Affero General Public License
-  - along with casper-icon.  If not, see <http://www.gnu.org/licenses/>.
+  - along with casper-icons.  If not, see <http://www.gnu.org/licenses/>.
   -
  */
 
@@ -24,11 +24,7 @@ import { CasperIcon } from './casper-icon.js';
 import {repeat} from 'lit/directives/repeat.js';
 
 /**
- * Little more than a glorified SVG element, with square aspect ratio,
- * that uses an in memory cache to keep a collection of often used SVG.
- * 
- * To improve loading performance of application's often used fa-awsome icons
- * are grouped into to three dynmically loaded predefined sets organized by icon family  
+ * Helper to display a gallery of all registered icons, organized by set
  */
 export class CasperIconGallery extends LitElement {
 
@@ -43,6 +39,7 @@ export class CasperIconGallery extends LitElement {
       grid-template-columns: repeat(auto-fill, 200px);
       column-gap: 10px;
       row-gap: 10px;
+      padding: 6px;
     }
 
     .icon-box {
@@ -65,33 +62,45 @@ export class CasperIconGallery extends LitElement {
       flex-align: end;
     }
 
+    h1 {
+      margin: 0px;
+      margin-top: 12px;
+    }
+
+    h2 {
+      color: #888;
+      margin: 0px;
+      font-size: 14px;
+    }
+
   `;
 
   constructor () {
     super();
-    const iconset = new Map();
+    const iconSets = new Map();
     CasperIcon._registry.forEach( (svg, name) => {
       let [setName, icon] = name.split(':');
-      if (!icon) setName = 'default';
-      let iset = iconset.get(setName);
+      if (!icon) setName = 'static';
+      let iset = iconSets.get(setName);
       if ( !iset ) {
-        iconset.set(setName, []);
-        iset = iconset.get(setName);
+        iconSets.set(setName, []);
+        iset = iconSets.get(setName);
       }
       iset.push(name);
     });
-    this.iconset = iconset;
+    this.iconSets = iconSets;
   }
 
   render () {
     return html`
       <div>
-        ${repeat(this.iconset.keys(), (iconSet) => html`
-          <h1>Icon set: ${iconSet}</h1>
+        ${repeat(this.iconSets.keys(), (iconSet) => html`
+          <h1>${ iconSet != 'static' ? `Icon set: ${iconSet}` : `From /static/icons`}</h1>
+          <h2>${this.iconSets.get(iconSet).length} icons</h2>
           <div class="gallery">
-            ${repeat(this.iconset.get(iconSet), (icon) => html`
+            ${repeat(this.iconSets.get(iconSet), (icon) => html`
               <div class="icon-box">
-                <span>${icon.split(':')[1]}</span>
+                <span>${this._nameHelper(icon)}</span>
                 <casper-icon icon=${icon}></casper-icon>
               </div>`)
             }
@@ -99,7 +108,16 @@ export class CasperIconGallery extends LitElement {
       </div>
     `;
   }
+
+  _nameHelper (name) {
+    let [setName, iconName] = name.split(':');
+    if ( iconName ) {
+      return iconName;
+    } else {
+      return setName;
+    }
+  }
+
 }
 
 window.customElements.define('casper-icon-gallery', CasperIconGallery);
-
